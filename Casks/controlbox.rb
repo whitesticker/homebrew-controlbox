dc03406f8715d1e6df361da1245ae47e6421281b
WhiteSticker <XC5028@gmail.com>
2026-09-12 00:29:13 -0700

    Update Control Box to 0.1.47

--- Casks/controlbox.rb
+++ Casks/controlbox.rb
@@ -1,6 +1,6 @@
 cask "controlbox" do
-  version "0.1.46"
-  sha256 "78a051151f5832b866df55dbe7e2155a4a1815c8ed61eb35b6fe1f486a073dfa"
+  version "0.1.47"
+  sha256 "5fd3ff7a4ca64e03c5fa65a31a02cb2c71d38729856480b15a9548ce0f0294e8"
 
   url "https://github.com/whitesticker/controlbox/releases/download/v#{version}/ControlBox-#{version}.zip"
   name "Control Box"
